a85a46dea8d54a94b64d5549f68c1f9791d7a836
Adminnn <admin@example.com>
2024-02-19 02:51:47 +0600

    ...

--- Product_Lists/views.py
+++ Product_Lists/views.py
@@ -5,6 +5,8 @@ def product_lists(request):
     
     products = Product.objects.order_by('-created_at')
     upcoming_products = UpComingProducts.objects.all()[:3] 
+    if upcoming_products.count() < 3:
+        upcoming_products = upcoming_products[:3]
 
     
     return render(request, 'pages/product_list.html', {'products': products, 'upcoming_products': upcoming_products})
